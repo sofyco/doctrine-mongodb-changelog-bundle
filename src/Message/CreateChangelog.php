932a1abc2afe850fe6b421fcc3ec6a4c34e2ee1a
Sofy Korzhova <sofykorzhova@gmail.com>
2023-02-19 16:51:41 +0100

    Added class-string phpdoc to document name

--- src/Message/CreateChangelog.php
+++ src/Message/CreateChangelog.php
@@ -4,7 +4,7 @@ namespace Sofyco\Bundle\Doctrine\MongoDB\ChangelogBundle\Message;
 
 final readonly class CreateChangelog
 {
-    public function __construct(public ?string $userIdentifier, public string $documentClassName, public string $documentId, public string $fieldName, public mixed $oldValue, public mixed $newValue)
+    public function __construct(public ?string $userIdentifier, /** @var class-string */ public string $documentClassName, public string $documentId, public string $fieldName, public mixed $oldValue, public mixed $newValue)
     {
     }
 }
